4400b90512f16a732127c61b4a52f50e7e274b17
shtosti <hanna.hubarava@gmail.com>
2024-12-02 11:52:53 +0100

    rename venv

---
+++ venv/lib/python3.12/site-packages/lexicalrichness/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for LexicalRichness."""
+
+__title__ = 'lexicalrichness'
+__author__ = """Lucas Shen YS"""
+__email__ = 'lucas@lucasshen.com'
+__version__ = '0.5.1'
+
+from .lexicalrichness import *
